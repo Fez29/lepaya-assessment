51394c87b6bff017b242b4e4c3a1fed5807c0c46
Gareth Fraser <41666396+Fez29@users.noreply.github.com>
2023-03-22 01:35:00 +0100

    commit s3 and rds module file

--- modules/platform/rds.tf
+++ modules/platform/rds.tf
@@ -1,21 +1,38 @@
-resource "random_password" "db_password" {
-  length  = 16
-  special = true
+data "aws_secretsmanager_random_password" "db_password" {
+  password_length            = 10
+  exclude_numbers            = false
+  require_each_included_type = true
+  include_space              = false
+  exclude_punctuation        = true
 }
 
 resource "aws_secretsmanager_secret" "db_password_secret" {
-  name = "${var.data.environment}-${var.common.project}-db_password_secret"
+  name = "${var.data.environment}_${var.common.project}_db_password_secret"
 }
+
+# Use in Production but requires setup of a Lambda
+# resource "aws_secretsmanager_secret_rotation" "example" {
+#   secret_id           = aws_secretsmanager_secret.db_password_secret.id
+#   rotation_lambda_arn = aws_lambda_function.example.arn
+
+#   rotation_rules {
+#     automatically_after_days = 30
+#   }
+# }
 
 resource "aws_secretsmanager_secret_version" "db_password_secret_version" {
   secret_id     = aws_secretsmanager_secret.db_password_secret.id
-  secret_string = random_password.db_password.result
+  secret_string = "${data.aws_secretsmanager_random_password.db_password.random_password}"
+  depends_on = [
+    aws_secretsmanager_secret.db_password_secret,
+    data.aws_secretsmanager_random_password.db_password,
+  ]
 }
 
 data "aws_secretsmanager_secret_version" "db_password_secret" {
-  secret_id = aws_secretsmanager_secret.db_password_secret.id
+  secret_id                  = aws_secretsmanager_secret.db_password_secret.id
   depends_on = [
-    aws_secretsmanager_secret.db_password_secret
+    aws_secretsmanager_secret_version.db_password_secret_version
   ]
 }
 
@@ -30,55 +47,65 @@ resource "aws_rds_cluster" "database" {
   engine             = "aurora-mysql"
   engine_mode        = "provisioned"
   engine_version     = "8.0.mysql_aurora.3.02.0"
-  database_name      = "${var.data.environment}${var.common.database_name}"
+  database_name      = "${var.data.environment}${var.data.environment_data.database_name}"
   availability_zones = var.data.availability_zones
   # TODO: change to using AWS secrets
   master_username = var.common.master_username
   #master_password = "password"
-  master_password           = data.aws_secretsmanager_secret_version.db_password_secret.secret_string
-  backup_retention_period   = 7
-  preferred_backup_window   = "03:00-04:00"
-  deletion_protection       = var.data.production == true ? true : false
-  skip_final_snapshot       = var.data.production == true ? true : false
-  final_snapshot_identifier = "${var.data.environment}-${var.common.project}-final-snapshot"
-  db_subnet_group_name      = aws_db_subnet_group.database.name
-  vpc_security_group_ids    = [aws_security_group.database_rds_sg.id]
+  master_password                     = "${data.aws_secretsmanager_secret_version.db_password_secret.secret_string}"
+  backup_retention_period             = 7
+  preferred_backup_window             = "03:00-04:00"
+  deletion_protection                 = var.data.production == true ? true : false
+  skip_final_snapshot                 = var.data.production == false ? true : false
+  final_snapshot_identifier           = "${var.data.environment}-${var.common.project}-final-snapshot"
+  db_subnet_group_name                = aws_db_subnet_group.database.name
+  vpc_security_group_ids              = [aws_security_group.database_rds_sg.id]
+  iam_database_authentication_enabled = true
+
 
   serverlessv2_scaling_configuration {
     max_capacity = 64.0 # Max 128
     min_capacity = 0.5
   }
+  depends_on = [
+    data.aws_secretsmanager_secret_version.db_password_secret
+  ]
 }
 
 resource "aws_rds_cluster_instance" "serverless_v2_mysql_instance" {
-  count                = 1
   cluster_identifier   = "${var.data.environment}${var.common.project}" #Use local value to ensure is not replace on each run
   instance_class       = "db.serverless"
   engine               = aws_rds_cluster.database.engine
   engine_version       = aws_rds_cluster.database.engine_version
-  publicly_accessible  = false
+  publicly_accessible  = true
   db_subnet_group_name = aws_db_subnet_group.database.name
+  apply_immediately    = var.data.production == false ? true : false
 
   tags = {
     Name = "${var.data.environment}-${var.common.project}-mysql-instance"
   }
+  depends_on = [
+    aws_rds_cluster.database
+  ]
 }
 
 resource "aws_security_group" "database_rds_sg" {
   name_prefix = "${var.data.environment}-${var.common.project}-rds-sg"
   vpc_id      = aws_vpc.main.id
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    # TODO: FIXME
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    # TODO: FIXME 
+    cidr_blocks = [data.aws_vpc.main.cidr_block]
+    # cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_db_subnet_group" "database" {
   name       = "${var.data.environment}-${var.common.project}-rds"
-  subnet_ids = data.aws_subnets.private_subnets.ids # Replace with your preferred subnets
+  # TODO: change back to private
+  subnet_ids = data.aws_subnets.public_subnets.ids # Replace with your preferred subnets
   depends_on = [
     data.aws_subnets.private_subnets
   ]
@@ -86,8 +113,8 @@ resource "aws_db_subnet_group" "database" {
 
 resource "aws_rds_cluster_parameter_group" "database" {
   name        = "${var.data.environment}-${var.common.project}-parameter-group"
-  family      = "aurora-mysql5.7"
-  description = "Aurora MySQL 5.7 parameter group"
+  family      = "aurora-mysql8.0"
+  description = "Aurora MySQL 8.0 parameter group"
   parameter {
     name  = "character_set_server"
     value = "utf8mb4"
--- modules/platform/s3.tf
+++ modules/platform/s3.tf
@@ -1,21 +1,21 @@
 resource "aws_s3_bucket" "bucket" {
 
-  bucket = "${var.common.project}-${var.common.s3_bucket}"
+  bucket = "${var.common.project}-${var.common.terraform_state_s3_bucket_prefix}-${var.data.environment}"
 
   versioning {
     enabled = true
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  # No longer required as all s3 buckets are now encrypted by default
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
 
   tags = {
-    Company = "Lepaya"
+    Company     = "Lepaya"
     Environment = var.data.environment
   }
 }
@@ -23,6 +23,10 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.s3_bucket_policy.json
+  depends_on = [
+    aws_s3_bucket.bucket,
+    data.aws_iam_policy_document.s3_bucket_policy,
+  ]
 }
 
 data "aws_iam_policy_document" "s3_bucket_policy" {
@@ -33,7 +37,28 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
     }
     effect = "Allow"
     actions = [
-      "s3:*",
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      "${aws_s3_bucket.bucket.arn}"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:sourceVpc"
+      values   = ["${aws_vpc.main.id}"]
+    }
+  }
+    statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
     resources = [
       "${aws_s3_bucket.bucket.arn}/*"
@@ -44,13 +69,4 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
       values   = ["${aws_vpc.main.id}"]
     }
   }
-}
-
-resource "aws_s3_bucket_object" "bucket" {
-  for_each = fileset(path.module, "data/*.json")
-  bucket   = aws_s3_bucket.bucket.id
-  key      = each.key
-  acl      = "private" # or can be "public-read"
-  source   = each.key
-  etag     = filemd5("${each.key}")
 }
